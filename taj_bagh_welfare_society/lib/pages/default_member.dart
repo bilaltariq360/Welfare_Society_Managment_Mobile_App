e2bbe12e600d2f700798a4950cbe5d392c3493f5
Bilal Tariq <hafizbilaltariq360@gmail.com>
2023-08-13 18:12:48 +0500

    Updated

--- taj_bagh_welfare_society/lib/pages/default_member.dart
+++ taj_bagh_welfare_society/lib/pages/default_member.dart
@@ -22,13 +22,15 @@ class DefaultMember extends StatefulWidget {
 class _DefaultMemberState extends State<DefaultMember> {
   int membersFoundIndex = 0;
 
+  List<String> filterMonth = ['All Records'];
+
+  List<Map<String, dynamic>> members = [];
+
   TextEditingController userController = TextEditingController();
 
   Future<bool> checkLinkExists(String url) async {
     try {
-      final response = await http.get(Uri.parse(url));
-      final extractedData = json.decode(response.body) as Map<String, dynamic>;
-      extractedData.forEach((fireBaseId, receipt) {});
+      final response = await http.head(Uri.parse(url));
       return (response.statusCode == 200);
     } catch (e) {
       return false;
@@ -39,16 +41,12 @@ class _DefaultMemberState extends State<DefaultMember> {
   void initState() {
     super.initState();
 
-    List<String> filterMonth = [];
-
-    List<Map<String, dynamic>> members = [];
-
     DateTime currentDate = DateTime.now();
-    DateTime startDate = DateTime(2023, 1); // Starting from August 2023
+    DateTime startDate = DateTime(2023, 1);
 
     while (startDate.isBefore(currentDate)) {
       String formattedDate = DateFormat.yMMM().format(startDate);
-      filterMonth.add(formattedDate);
+      filterMonth.insert(1, formattedDate);
       startDate = DateTime(startDate.year, startDate.month + 1);
     }
 
@@ -71,7 +69,8 @@ class _DefaultMemberState extends State<DefaultMember> {
       });
     }).then((value) async {
       for (var i = 0; i < members.length; i++) {
-        for (var j = 0; j < filterMonth.length; j++) {
+        bool memberFound = false;
+        for (var j = 1; j < filterMonth.length; j++) {
           String month = filterMonth[j].replaceAll(' ', ''),
               street = members[i]['Street'].replaceAll(' ', ''),
               houseNo = members[i]['House No'].replaceAll(' ', '');
@@ -81,6 +80,10 @@ class _DefaultMemberState extends State<DefaultMember> {
           bool link = await checkLinkExists(url);
 
           if (!link) {
+            if (!memberFound) {
+              membersFoundIndex++;
+              memberFound = true;
+            }
             members[i]['Default Months'].add(filterMonth[j]);
           }
         }
@@ -91,15 +94,6 @@ class _DefaultMemberState extends State<DefaultMember> {
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<UserProvider>(context);
-    List<ReceiptData> receipts = [];
-    if (MyRecord.receiptMonth == 'All Records') {
-      receipts = provider.receiptData;
-    } else {
-      receipts = provider.receiptData
-          .where((receipt) =>
-              DateFormat.yMMM().format(receipt.date) == MyRecord.receiptMonth)
-          .toList();
-    }
 
     provider.checkConnection();
     return (!provider.connected)
@@ -171,7 +165,7 @@ class _DefaultMemberState extends State<DefaultMember> {
                     const SizedBox(height: 25),
                     MyDropdown(
                         hintText: 'All Records',
-                        list: [],
+                        list: filterMonth,
                         prefixIcon: CupertinoIcons.calendar_today),
                     const SizedBox(height: 25),
                     Row(
@@ -224,140 +218,145 @@ class _DefaultMemberState extends State<DefaultMember> {
                               fontWeight: FontWeight.bold),
                         ),
                         const SizedBox(height: 10),
-                        /*...members[0].map((member) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 30, vertical: 20),
-                            child: GestureDetector(
-                              child: Container(
-                                padding: const EdgeInsets.all(15),
-                                height: 350,
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                    color: Style.themeUltraLight,
-                                    borderRadius: BorderRadius.circular(10)),
-                                child: Column(
-                                  children: [
-                                    Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: [
-                                        const SizedBox(width: 10),
-                                        Text(
-                                          'Member Details',
-                                          style: TextStyle(
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.bold,
-                                              color: Style.themeDark),
-                                        ),
-                                      ],
+                        ...members.map((member) {
+                          return (member['Default Months'].isEmpty)
+                              ? const SizedBox()
+                              : Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 30, vertical: 20),
+                                  child: GestureDetector(
+                                    child: Container(
+                                      padding: const EdgeInsets.all(15),
+                                      height: 350,
+                                      width: double.infinity,
+                                      decoration: BoxDecoration(
+                                          color: Style.themeUltraLight,
+                                          borderRadius:
+                                              BorderRadius.circular(10)),
+                                      child: Column(
+                                        children: [
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: [
+                                              const SizedBox(width: 10),
+                                              Text(
+                                                'Member Details',
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Style.themeDark),
+                                              ),
+                                            ],
+                                          ),
+                                          const SizedBox(height: 30),
+                                          Row(
+                                            children: [
+                                              Icon(Icons.person,
+                                                  color: Style.themeDark),
+                                              const SizedBox(width: 10),
+                                              Text(
+                                                member['Full Name'],
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Style.themeDark),
+                                              ),
+                                            ],
+                                          ),
+                                          const SizedBox(height: 15),
+                                          Row(
+                                            children: [
+                                              Icon(
+                                                  CupertinoIcons
+                                                      .doc_text_viewfinder,
+                                                  color: Style.themeDark),
+                                              const SizedBox(width: 10),
+                                              Text(
+                                                member['CNIC'],
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Style.themeDark),
+                                              ),
+                                            ],
+                                          ),
+                                          const SizedBox(height: 15),
+                                          Row(
+                                            children: [
+                                              Icon(CupertinoIcons.phone_fill,
+                                                  color: Style.themeDark),
+                                              const SizedBox(width: 10),
+                                              Text(
+                                                member['Mobile'],
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Style.themeDark),
+                                              ),
+                                            ],
+                                          ),
+                                          const SizedBox(height: 15),
+                                          Row(
+                                            children: [
+                                              Icon(Icons.add_road,
+                                                  color: Style.themeDark),
+                                              const SizedBox(width: 10),
+                                              Text(
+                                                '${member['Street']} street',
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Style.themeDark),
+                                              ),
+                                            ],
+                                          ),
+                                          const SizedBox(height: 15),
+                                          Row(
+                                            children: [
+                                              Icon(CupertinoIcons.number,
+                                                  color: Style.themeDark),
+                                              const SizedBox(width: 10),
+                                              Text(
+                                                'House No. ${member['House No']}',
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Style.themeDark),
+                                              ),
+                                            ],
+                                          ),
+                                          const SizedBox(height: 15),
+                                          Row(
+                                            children: [
+                                              Icon(
+                                                  CupertinoIcons
+                                                      .rectangle_arrow_up_right_arrow_down_left,
+                                                  color: Style.themeDark),
+                                              const SizedBox(width: 10),
+                                              Text(
+                                                member['House Area'],
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Style.themeDark),
+                                              ),
+                                            ],
+                                          ),
+                                          const SizedBox(height: 15),
+                                          Row(
+                                            children: [
+                                              Icon(CupertinoIcons.house,
+                                                  color: Style.themeDark),
+                                              const SizedBox(width: 10),
+                                              Text(
+                                                member['House Property'],
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Style.themeDark),
+                                              ),
+                                            ],
+                                          ),
+                                        ],
+                                      ),
                                     ),
-                                    const SizedBox(height: 30),
-                                    Row(
-                                      children: [
-                                        Icon(Icons.person,
-                                            color: Style.themeDark),
-                                        const SizedBox(width: 10),
-                                        Text(
-                                          member[1],
-                                          style: TextStyle(
-                                              fontSize: 16,
-                                              color: Style.themeDark),
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 15),
-                                    Row(
-                                      children: [
-                                        Icon(CupertinoIcons.doc_text_viewfinder,
-                                            color: Style.themeDark),
-                                        const SizedBox(width: 10),
-                                        Text(
-                                          member[0],
-                                          style: TextStyle(
-                                              fontSize: 16,
-                                              color: Style.themeDark),
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 15),
-                                    Row(
-                                      children: [
-                                        Icon(CupertinoIcons.phone_fill,
-                                            color: Style.themeDark),
-                                        const SizedBox(width: 10),
-                                        Text(
-                                          member[2],
-                                          style: TextStyle(
-                                              fontSize: 16,
-                                              color: Style.themeDark),
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 15),
-                                    Row(
-                                      children: [
-                                        Icon(Icons.add_road,
-                                            color: Style.themeDark),
-                                        const SizedBox(width: 10),
-                                        Text(
-                                          '${member[3]} street',
-                                          style: TextStyle(
-                                              fontSize: 16,
-                                              color: Style.themeDark),
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 15),
-                                    Row(
-                                      children: [
-                                        Icon(CupertinoIcons.number,
-                                            color: Style.themeDark),
-                                        const SizedBox(width: 10),
-                                        Text(
-                                          'House No. ${member[6]}',
-                                          style: TextStyle(
-                                              fontSize: 16,
-                                              color: Style.themeDark),
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 15),
-                                    Row(
-                                      children: [
-                                        Icon(
-                                            CupertinoIcons
-                                                .rectangle_arrow_up_right_arrow_down_left,
-                                            color: Style.themeDark),
-                                        const SizedBox(width: 10),
-                                        Text(
-                                          member[4],
-                                          style: TextStyle(
-                                              fontSize: 16,
-                                              color: Style.themeDark),
-                                        ),
-                                      ],
-                                    ),
-                                    const SizedBox(height: 15),
-                                    Row(
-                                      children: [
-                                        Icon(CupertinoIcons.house,
-                                            color: Style.themeDark),
-                                        const SizedBox(width: 10),
-                                        Text(
-                                          member[5],
-                                          style: TextStyle(
-                                              fontSize: 16,
-                                              color: Style.themeDark),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          );
-                        }),*/
+                                  ),
+                                );
+                        }),
                       ],
                     )
                   ],
